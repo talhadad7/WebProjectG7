71c91f80bc4d4a2cd239e71e87b265ef47c05048
Tal Hadad <hadad4@post.bgu.ac.il>
2026-01-07 00:36:49 +0200

    ConectForm

--- ServerSide/Index.js
+++ ServerSide/Index.js
@@ -1,14 +1,27 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.urlencoded({ extended: true })); // בשביל form רגיל (application/x-www-form-urlencoded)
+app.use(express.json()); // בשביל JSON
+
 app.get("/", (req, res) => {
-  res.send("Server Is Running");
+  res.send("Server Is Runnding");
 });
 
+app.post("/contact", (req, res) => {
+  const { name, email, subject, message } = req.body;
 
+  console.log("New contact:", req.body);
+
+  res.send(`
+    <h1>Contact received</h1>
+    <p><b>Name:</b> ${name}</p>
+    <p><b>Email:</b> ${email}</p>
+    <p><b>Subject:</b> ${subject}</p>
+    <p><b>Message:</b> ${message}</p>
+    <a href="javascript:history.back()">Back</a>
+  `);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
