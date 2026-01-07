350e4324f92fe61bb73c5c2c1f1f37e94bb0dc22
Tal Hadad <hadad4@post.bgu.ac.il>
2026-01-07 12:15:32 +0200

    ConnectToServer

--- ServerSide/Index.js
+++ ServerSide/Index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true })); // בשביל form רגיל (application/x-www-form-urlencoded)
 app.use(express.json()); // בשביל JSON
 const cors = require("cors");
 app.use(cors());
+app.use(express.static(path.join(__dirname, "..", "ClientSide")));
+
 app.get("/", (req, res) => {
-  res.send("Server Is Running");
+  res.sendFile(path.join(__dirname, "..", "ClientSide", "index.html"));
 });
 
 app.post("/contact", (req, res) => {
